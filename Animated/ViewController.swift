4149014fe0f3e1674e2ede5a8d6439bddfd220d4
weutnyc <weutnyc@gmail.com>
2021-04-27 13:19:47 +0300

    Initial Commit

---
+++ Animated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animated
+//
+//  Created by Anton on 27.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
